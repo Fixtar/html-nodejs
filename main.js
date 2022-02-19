211bd4b1f3d0cc24ff21f74b3dcb735116e087e9
seongha_h <58902013+Fixtar@users.noreply.github.com>
2022-02-20 02:29:33 +0900

    데이터를 받아와서 동적으로 리스트 생성

--- main.js
+++ main.js
@@ -3,38 +3,85 @@ var fs = require('fs');
 var url = require('url');
 
 var app = http.createServer(function (request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    console.log(queryData);
-    if (_url == '/') {
-        _url = '/index.html';
-    }
-    if (_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf-8', (err, data) => {
-        var content = `<!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${data}</title>
-        <meta charset="utf-8">
-        </head>
-        
-        <body>
-          <h1><a href="index.html">WEB</a><h1>
-        <ol> <!-- 리스트 부모태그 ul 연관성을 나타내기 위해서 사용 ol은 자동으로 숫자를 넘버링 해줌 개꿀임-->
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=Javascript">JavaScript</a></li>
-        </ol>
-        <h2>WEB</h2>
-        <p>${data}</p>
-        </body>
-        </html>
-        `;
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+
+      fs.readdir('./data', (err, fileList) => {
+        var title = 'Welcome';
+        var data = 'Hello Node.js';
+        var List = '<ul>';
+        var i = 0;
+        while (i < fileList.length) {
+          List += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+          i = i + 1;
+        }
+        List += '</ul>';
+
+        var content = `
+                  <!doctype html>
+                  <html>
+                  <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                  </head>
+                  
+                  <body>
+                    <h1><a href="/">WEB</a><h1>
+                  ${List}
+                  <h2>${title}</h2>
+                  <p>${data}</p>
+                  </body>
+                  </html>
+                  `;
+        response.writeHead(200);
         response.end(content);
-    })
+      })
+
+    }
+    else {
+      fs.readdir('./data', (err, fileList) => {
+        var title = queryData.id;
+        var List = '<ul>';
+        var i = 0;
+        while (i < fileList.length) {
+          List += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+          i += 1;
+        }
+        List += '</ul>';
+        fs.readFile(`data/${queryData.id}`, 'utf-8', (err, data) => {
+          var title = queryData.id;
+          var content = `
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            
+            <body>
+              <h1><a href="/">WEB</a><h1>
+            ${List}
+            <h2>${title}</h2>
+            <p>${data}</p>
+            </body>
+            </html>
+            `;
+          response.writeHead(200);
+          response.end(content);
+        });
+      });
+    }
+  }
+  else {
+    response.writeHead(404);
+    response.end('Not Found');
+  }
+
+
 
 });
 app.listen(3000);

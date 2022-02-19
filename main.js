7a292012929d8b82c1e7f058adbcd2d6e7e03fb0
seongha_h <58902013+Fixtar@users.noreply.github.com>
2022-02-19 17:00:07 +0900

    fs (파일시스템)을 이용하여, id값을 통한 데이터파일 연결 및 사용

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function (request, response) {
-    var url = request.url;
-    if (request.url == '/') {
-        url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData);
+    if (_url == '/') {
+        _url = '/index.html';
     }
-    if (request.url == '/favicon.ico') {
+    if (_url == '/favicon.ico') {
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    fs.readFile(`data/${queryData.id}`, 'utf-8', (err, data) => {
+        var content = `<!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${data}</title>
+        <meta charset="utf-8">
+        </head>
+        
+        <body>
+          <h1><a href="index.html">WEB</a><h1>
+        <ol> <!-- 리스트 부모태그 ul 연관성을 나타내기 위해서 사용 ol은 자동으로 숫자를 넘버링 해줌 개꿀임-->
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=Javascript">JavaScript</a></li>
+        </ol>
+        <h2>WEB</h2>
+        <p>${data}</p>
+        </body>
+        </html>
+        `;
+        response.end(content);
+    })
 
 });
 app.listen(3000);

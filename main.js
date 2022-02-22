60fef287875e1400e4f60b8769983a75dfd8dc4b
seongha_h <58902013+Fixtar@users.noreply.github.com>
2022-02-23 03:56:48 +0900

    post 방식으로 데이터 전송, 받기,  받은 데이터를 파일로 만들어 생성

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const { title } = require('process');
 
 function templateHTML(title, list, body) {
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -37,18 +39,15 @@ var app = http.createServer(function (request, response) {
 
   if (pathname === '/') {
     if (queryData.id === undefined) {
-
       fs.readdir('./data', (err, fileList) => {
         var title = 'Welcome';
         var data = 'Hello Node.js';
         var list = templateList(fileList);
         var template = templateHTML(title, list, `<h2>${title}</h2>${data}`);
 
-
         response.writeHead(200);
         response.end(template);
       })
-
     }
     else {
       fs.readdir('./data', (err, fileList) => {
@@ -62,6 +61,43 @@ var app = http.createServer(function (request, response) {
         });
       });
     }
+  }
+  else if (pathname === '/create') {
+    fs.readdir('./data', (err, fileList) => {
+      var title = 'Welcome - create';
+      var list = templateList(fileList);
+      var template = templateHTML(title, list,
+        `
+        <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"/></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit" />
+          </p>
+        </form>
+         `
+      );
+
+      response.writeHead(200);
+      response.end(template);
+    })
+  }
+  else if (pathname === '/create_process') {
+    var body = "";
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      var post = new URLSearchParams(body);
+      var title = post.get("title");
+      var description = post.get("description");
+      fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end();
+      })
+    });
   }
   else {
     response.writeHead(404);
